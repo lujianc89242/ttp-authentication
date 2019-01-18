3dddbd60ccd374d49a445a6f7a8fb3c18226ab6f
jc <rogerfrontend@gmail.com>
2019-01-17 22:02:06 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = new express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, world');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
